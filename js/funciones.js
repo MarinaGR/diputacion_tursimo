a3ac89757bf8415039a98472c90569b2c7f22ed5
MarinaGR <marinagr1984@gmail.com>
2016-12-13 19:19:59 +0100

    update notif

--- js/funciones.js
+++ js/funciones.js
@@ -423,7 +423,7 @@ function onNotification(e) {
 							case "mensaje":
 							default:
 										navigator.notification.alert(
-											notif.message,  			// message
+											notif.data.mensaje,  		// message
 											alertDismissed,   			// callback
 											'Nuevo mensaje: '+notif.title,   // title
 											'OK'              			// buttonName
@@ -467,7 +467,7 @@ function onNotification(e) {
 							case "mensaje":
 							default:
 										navigator.notification.alert(
-											notif.message,  			// message
+											notif.data.mensaje,  		// message
 											alertDismissed,   			// callback
 											'Nuevo mensaje: '+notif.title,   // title
 											'OK'              			// buttonName
@@ -5620,7 +5620,7 @@ function ajax_recover_extern_data(operation, container, params) {
 							var texto_compartir="¿Te interesa este evento? "+titulo_compartir+" - LUGAR: "+lugar_compartir+" - (*) Descarga la aplicación ·Diputación de Ávila - Turismo· desde Google Play para Android o desde App Store para Iphone y mantente informado."
 						
 							cadena+='<div class="" id="compartir" onclick="window.plugins.socialsharing.share(\''+texto_compartir+'\', \''+titulo_compartir+'\', \'\', null)" >'+
-								'<div class="ov_text_08"><i class="fa fa-share-alt fa-fw fa-lg"> </i> COMPARTIR ESTE EVENTO</div></div>';
+								'<div class="ov_text_23"><i class="fa fa-share-alt fa-fw fa-lg"> </i> COMPARTIR ESTE EVENTO</div></div>';
 														
 							cadena+='<div class="ov_clear_floats_01"> </div>';
 
